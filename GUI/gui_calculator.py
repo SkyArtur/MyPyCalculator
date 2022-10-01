cff5fd2e65f86198723e78bb87121d78817fde91
Artur <93395366+SkyArtur@users.noreply.github.com>
2022-10-01 14:08:06 -0300

    refatoração das clases CalcIcon e CalcDisplayFont.

--- GUI/gui_calculator.py
+++ GUI/gui_calculator.py
@@ -18,11 +18,11 @@ class MyPyCalculator(CalcWidget):
 
         :return: None
         """
-        grid = [["7", "8", "9", "C", "Del"], ["4", "5", "6", "+", "/"], ["1", "2", "3", "-", "*"]]
+        btn = [["7", "8", "9", "C", "Del"], ["4", "5", "6", "+", "/"], ["1", "2", "3", "-", "*"]]
         for pos in range(5):
-            self.keyboard.grid.addWidget(CalcButton(self.keyboard, f'{grid[0][pos]}'), 1, pos, 1, 1)
-            self.keyboard.grid.addWidget(CalcButton(self.keyboard, f'{grid[1][pos]}'), 2, pos, 1, 1)
-            self.keyboard.grid.addWidget(CalcButton(self.keyboard, f'{grid[2][pos]}'), 3, pos, 1, 1)
+            self.keyboard.grid.addWidget(CalcButton(self.keyboard, f'{btn[0][pos]}'), 1, pos, 1, 1)
+            self.keyboard.grid.addWidget(CalcButton(self.keyboard, f'{btn[1][pos]}'), 2, pos, 1, 1)
+            self.keyboard.grid.addWidget(CalcButton(self.keyboard, f'{btn[2][pos]}'), 3, pos, 1, 1)
         self.keyboard.grid.addWidget(CalcButton(self.keyboard, '.'), 4, 0, 1, 1)
         self.keyboard.grid.addWidget(CalcButton(self.keyboard, '0'), 4, 1, 1, 1)
         self.keyboard.grid.addWidget(CalcButton(self.keyboard, '='), 4, 2, 1, 3)
